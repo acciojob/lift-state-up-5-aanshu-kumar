8234d0dbae1f9cff47254dd3654f82b5df28cad4
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2025-01-09 17:57:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,29 @@
 
 import React, { useState } from "react";
 import './../styles/App.css';
-import Form from "./Form"
 
 const App = () => {
   const [isLoggedIn,setIsLoggedIn] = useState(false);
   return (
     <div>
-      
+      <h1>Parent Componenet</h1>
       {!isLoggedIn ? <Form isLoggedIN={isLoggedIn} setIsLoggedIn = {setIsLoggedIn}></Form> : <p>You are logged in!</p> }
       
     </div>
   )
+}
+
+function Form({isLoggedIN, setIsLoggedIn})
+{
+  return<div>   
+    <label for="username">Username:</label>
+    <input id="username" type="text"></input><br/><br/>
+    <label for="password">Password:</label>
+    <input id="password" type="password"></input><br/><br/>
+    <button onClick={()=>{
+    setIsLoggedIn(!isLoggedIN)
+  }}>Login</button>
+  </div> 
 }
 
 export default App
