c755a0e76c1933f5407f2435a11e1bb01aa0fc89
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2025-01-09 17:46:20 +0530

    Create Form.js

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from "react"
+export default function Form({isLoggedIN, setIsLoggedIn})
+{
+
+  return<div>
+    <label for="username">Username:</label>
+    <input id="username" type="text"></input><br/><br/>
+    <label for="password">Password:</label>
+    <input id="password" type="password"></input><br/><br/>
+    <button onClick={()=>{
+    setIsLoggedIn(!isLoggedIN)
+  }}>Login</button>
+  </div> 
+}
