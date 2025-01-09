b9ea1187a4f74affc00c9d9bc046bfc7ae22973a
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2025-01-09 17:45:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Form from "./Form"
 
 const App = () => {
+  const [isLoggedIn,setIsLoggedIn] = useState(false);
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Parent Componenet</h1>
+      {!isLoggedIn ? <Form isLoggedIN={isLoggedIn} setIsLoggedIn = {setIsLoggedIn}></Form> : <p>You are logged in!</p> }
+      
     </div>
   )
 }
 
 export default App
+
+
