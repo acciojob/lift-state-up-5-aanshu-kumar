945ba9e9ece8e58664a33a276ab605929539d5e2
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2025-01-09 17:52:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const [isLoggedIn,setIsLoggedIn] = useState(false);
   return (
     <div>
-      <h1>Parent Componenet</h1>
+      
       {!isLoggedIn ? <Form isLoggedIN={isLoggedIn} setIsLoggedIn = {setIsLoggedIn}></Form> : <p>You are logged in!</p> }
       
     </div>
